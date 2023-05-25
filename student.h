90c7b770deec163d75d8209a38c3ed5ce12c4633
ivanaaljubas <132484498+ivanaaljubas@users.noreply.github.com>
2023-05-25 17:16:33 +0200

    Add files via upload

---
+++ student.h
@@ -0,0 +1,20 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#define MAX_STUDENTS 100
+
+typedef struct {
+	char ime[50];
+	char prezime[50];
+	char mjestoStanovanja[100];
+	float prosjek;
+} Student;
+
+void unosStudenta(Student* studenti, int* brojStudenata);
+void ispisSvihStudenata(Student* studenti, int brojStudenata);
+void pretraziStudenta(Student* studenti, int brojStudenata, const char* prezime);
+void obrisiStudenta(Student* studenti, int* brojStudenata, const char* prezime);
+void spremiStudente(Student* studenti, int brojStudenata, const char* nazivDatoteke);
+void ucitajStudente(Student* studenti, int* brojStudenata, const char* nazivDatoteke);
+
+#endif
